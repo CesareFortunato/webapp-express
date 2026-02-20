d08873bedbcd169ea2dac51dd8ca6aa033d8761f
CesareFortunato <cesare.fortunato89@gmail.com>
2026-02-20 17:34:30 +0100

    feat: add controller

---
+++ controller/filmController.js
@@ -0,0 +1,20 @@
+// importo la connessione del DB
+const connection = require('../data/db');
+
+
+//funzione index
+
+function index(req, res) {
+    
+}
+
+
+
+//funzione show
+
+function show(req, res) {
+
+}
+
+
+modeul.exports = { index, show }
